152c8f1845944f71ce5a26af41d794f33fc42bde
Kontopodis Emmanouil <kontopodis.em@gmail.com>
2026-01-25 17:01:54 +0200

    styling home url

--- equities/views.py
+++ equities/views.py
@@ -25,7 +25,7 @@ def index(request):
             "description": ticker["description"],
         })
     context = {"data": charts}
-    print(context)
+
     return HttpResponse(template.render(context, request))
 
 
@@ -47,21 +47,34 @@ def strategies(request):
             "source": ticker["name"] + ".png",
         })
     context = {"data": charts}
-    print(CACHE.controller)
+
     return HttpResponse(template.render(context, request))
 
 
 def home(request):
     template = loader.get_template("home.html")
-    equities_data = Equity.objects.all()
+    equities_data = CACHE.controller
     data = []
+
+
     for n in equities_data:
 
+        last_price = n["data"].tail(1)
+        print(last_price['Close'])
         temp = {
-            "id": n.id,
-            "name": n.name,
-            "ticker": n.ticker,
-            "description": n.description,
+            "id": n["id"],
+            "name": n["name"],
+            "ticker": n['ticker'],
+            "description": n['description'],
+            "close_price":round(last_price.iloc[0]['Close'],2),
+            "open_price": round(last_price.iloc[0]['Open'],2),
+            "low_price": round(last_price.iloc[0]['Low'],2),
+            "high_price": round(last_price.iloc[0]['High'],2),
+            "vwap_high_price": round(last_price.iloc[0]['Vwap_high'], 2),
+            "vwap_low_price": round(last_price.iloc[0]['Vwap_low'], 2),
+            "ma50_price": round(last_price.iloc[0]['Ma_50'], 2),
+            "ma100_price": round(last_price.iloc[0]['Ma_100'], 2),
+            "ma200_price": round(last_price.iloc[0]['Ma_200'], 2),
         }
         data.append(temp)
     context = {"data": data}
@@ -73,7 +86,6 @@ def equity(request, id):
     all_equities = CACHE.controller
     data = {"name":"Wrong Id"}
     for e in all_equities:
-        print( e["id"], id)
         if id == e["id"]:
             data = e
             data["data"].cumsum()
